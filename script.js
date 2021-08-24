e710d2b01e87a8fece4eac145b9d81726e272e57
Dani Botelho <danibl26@gmail.com>
2021-08-24 11:33:27 -0300

    codigo refatorado

--- script.js
+++ script.js
@@ -1,56 +1,51 @@
-const obj = {
-    pessoaA: {
-        nome: 'Dani',
-        idade: 27,
-        peso: 75,
-        altura: 1.67
-    },
+class Pessoa{
+    constructor(nome, idade, peso, altura){
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    calculaIMC = (peso , altura) => {
+        let result= (this.peso / (this.altura * this.altura)).toFixed(2);
+        console.log(`${this.nome}, Seu IMC é : ${result}`)
+        
+        if (result < 18.5) {
+            console.log('Classificação: Magreza - Obesidae Grau: 0 ')
+        } else if ( result <=24.9){
+            console.log('Classificação: NORMAL - Obesidae Grau: 0 ');
+        } else if ( result <= 29.9){
+            console.log('Classificação: SOBREPESO - Obesidae Grau: I ')
+        } else if (result <=39.9) {
+            console.log('Classificação: OBESIDADE - Obesidae Grau: II ');
+        } else if (result > 40) {
+            console.log('Classificação: OBESIDADE GRAVE - Obesidae Grau: III')
+        } else {
+            console.log('não foi possivel ter uma resposta')
+        }
+        
+            return result
+        }
+
+
+     anoNascimento = (idade) => {
+            let anoAtualizado = new Date();
+            let ano = anoAtualizado.getFullYear() - this.idade;
+            console.log(`Você nasceu no ano de: ${ano}`);
+            
+          return ano;
+        }
+
+    }  
+
+
+
+  const  pessoaA = new Pessoa('Dani', 27, 75, 1.67);
     
-    pessoaB: {
-        nome: 'Lara',
-        idade: 27,
-        peso: 51,
-        altura: 1.58
-    }
-}
-
-let calculaIMC = (peso , altura) => {
-    let result= (peso / (altura * altura)).toFixed(2);
-    console.log(`Seu IMC é : ${result}`)
-
-    if (result < 18.5) {
-        console.log('Classificação: Magreza - Obesidae Grau: 0 ')
-    } else if ( result <=24.9){
-        console.log('Classificação: NORMAL - Obesidae Grau: 0 ');
-    } else if ( result <= 29.9){
-        console.log('Classificação: SOBREPESO - Obesidae Grau: I ')
-    } else if (result <=39.9) {
-        console.log('Classificação: OBESIDADE - Obesidae Grau: II ');
-    } else if (result > 40) {
-        console.log('Classificação: OBESIDADE GRAVE - Obesidae Grau: III')
-    } else {
-        console.log('não foi possivel ter uma resposta')
-    }
-
-        return result
-    }
-
-   
-
-let anoNascimento = (idade) => {
-    let anoAtualizado = new Date();
-    let ano = anoAtualizado.getFullYear() - idade;
-    console.log(`Você nasceu no ano de: ${ano}`);
-
-    return ano;
-}    
-
-//Pessoa A
-console.log(obj.pessoaA)
-calculaIMC(obj.pessoaA.peso , obj.pessoaA.altura)
-anoNascimento(obj.pessoaA.idade)
-
-//Pessoa B
-console.log(obj.pessoaB)
-calculaIMC(obj.pessoaB.peso , obj.pessoaB.altura)
-anoNascimento(obj.pessoaB.idade)
+    pessoaA.calculaIMC()
+    pessoaA.anoNascimento
+    
+  const  pessoaB = new Pessoa('Lara', 27, 51, 1.58) 
+    
+    pessoaB.calculaIMC()
+    pessoaB.anoNascimento()
